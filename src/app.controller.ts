29410495a561c605828a854c757157aabe351e12
aliraza291 <alirazajutt5412@gmail.com>
2025-07-23 09:28:13 -0700

    minor fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { SqsConsumerService } from './services/sqs-consumer';
 
-@Controller('health')
+@Controller('poll')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
