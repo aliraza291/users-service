ae4bb141eb8dabe954893c45fc3078d5b4b09823
aliraza291 <alirazajutt5412@gmail.com>
2025-07-22 23:31:33 -0700

    fix ui

--- dist/main.js
+++ dist/main.js
@@ -2,29 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const platform_express_1 = require("@nestjs/platform-express");
-const express = require("express");
-let cachedApp;
 async function bootstrap() {
-    if (!cachedApp) {
-        const expressApp = express();
-        const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(expressApp));
-        app.enableCors();
-        await app.init();
-        cachedApp = expressApp;
-    }
-    return cachedApp;
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    await app.listen(process.env.PORT ?? 3001);
 }
-if (process.env.NODE_ENV !== 'production') {
-    bootstrap().then(app => {
-        const port = process.env.PORT || 3001;
-        app.listen(port, () => {
-            console.log(`Application is running on port ${port}`);
-        });
-    });
-}
-exports.default = async (req, res) => {
-    const app = await bootstrap();
-    return app(req, res);
-};
+bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-
-let cachedApp: any;
 
 async function bootstrap() {
-  if (!cachedApp) {
-    const expressApp = express();
-    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-    
-    // Enable CORS if needed
-    app.enableCors();
-    
-    await app.init();
-    cachedApp = expressApp;
-  }
-  return cachedApp;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3001);
 }
-
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  bootstrap().then(app => {
-    const port = process.env.PORT || 3001;
-    app.listen(port, () => {
-      console.log(`Application is running on port ${port}`);
-    });
-  });
-}
-
-// Export for Vercel serverless
-export default async (req: any, res: any) => {
-  const app = await bootstrap();
-  return app(req, res);
-};
+bootstrap();
